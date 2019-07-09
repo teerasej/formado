b32ddb44ae8c52ca8ac54085fd94dd4305f4f1e2
Teerasej <teerasej@gmail.com>
2019-07-09 23:03:32 +0700

    Filter for USB drive only and show logs

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const drivelist = require('drivelist');
 
 showDrive = async () => { 
-    const drives = await drivelist.list();
-    console.log(drives);
+    const drives = await drivelist.list()
+    
+
+    const usbDrives = drives.filter((drive) => {
+        if(drive.isUSB) return true;
+        return false;
+    });
+
+    console.log(usbDrives);
+    
+    if(usbDrives.length > 0){
+        console.log(`Found ${usbDrives.length} drives`);
+    }
+    
+
 }
 
 showDrive();
+
